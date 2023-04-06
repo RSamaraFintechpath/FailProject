d1a8d1f249ed75f9b2f8456eae93db9e6aa6c154
Rami Samara <rsamara@fintechpath.com>
2023-04-06 10:34:28 +0300

    initial commit

---
+++ src/main/java/com/example/demo/FailProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailProjectApplication.class, args);
+	}
+
+	public int calculateSomething(int x) {
+		return x + 3;
+	}
+}
