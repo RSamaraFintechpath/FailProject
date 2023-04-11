a39bf8f5f0bb51065802bf6281aab09d4bb3fc54
Rami Samara <rsamara@fintechpath.com>
2023-04-11 11:41:23 +0300

    12312

--- src/main/java/com/example/demo/FailProjectApplication.java
+++ src/main/java/com/example/demo/FailProjectApplication.java
@@ -9,7 +9,7 @@ public class FailProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FailProjectApplication.class, args);
 	}
-
+    
 	public int calculateSomething(int x) {
 		return x + 3;
 	}
