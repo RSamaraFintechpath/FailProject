d26cd97f9620957cad07bedfcf476afa6b8dbe28
Rami Samara <rsamara@fintechpath.com>
2023-04-11 13:28:24 +0300

    123124

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,11 +26,6 @@ public class UserController {
 
 		this.userService = userService;
 	}
-
-//	@PostMapping(value = "/newUser", consumes = "application/json", produces = "application/json")
-//	public UserDto addUser(@RequestBody UserDto x) {
-//		return userService.addUser(x);
-//	}
 
 	@ApiParam(value = "the id that you insterted previously in the add endpoint", required = true)
 	@GetMapping(value = "/findby/{id}", produces = "application/json")
