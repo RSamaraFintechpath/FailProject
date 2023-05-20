5d76cb7a8a529a7045095fcc9f1f979e68226929
Rami Samara <rsamara@fintechpath.com>
2023-05-20 19:45:06 +0300

    removed unsed import

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
